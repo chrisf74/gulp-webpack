571a79e6dc7dec3f7b9f4e6eb32a6ed6e8b91d26
Chris Forrest <chrisf74@gmail.com>
2016-03-01 17:11:03 -0500

    JSX src

--- config/getWebpackConfig.js
+++ config/getWebpackConfig.js
@@ -4,13 +4,26 @@ var srcPath = path.resolve(__dirname, '../src');
 module.exports = function () {
 	return {
 		entry: {
-			app: [srcPath + '/index.js']
+			app: [srcPath + '/index.jsx']
 		},
 
 		devServer: {
 			publicPath: '/',
 			filename: 'srcBundle.js',
 			contentBase: 'src/'
+		},
+
+		module: {
+			loaders: [
+			  {
+			    test: /\.jsx?$/,
+			    exclude: /(node_modules|bower_components)/,
+			    loader: 'babel',
+			    query: {
+			      presets: ['es2015']
+			    }
+			  }
+			]
 		},
 
 		plugins: []
---
+++ src/index.jsx
@@ -0,0 +1,2 @@
+import add from './add.jsx';
+console.log(add(1,2));
